f7c6bdcc4300a36e283034bed24b81a808574dec
Luke McMillan <mcmillan.luke@yahoo.com.au>
2021-09-29 22:05:11 +1000

    added moment script for the 5 day forecast to the JS file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,8 @@ var today = moment().format("dddd, MMMM Do");
 $("#currentDate").prepend(today);
 console.log(today);
 
+// Date for the 5 day forecast
+
+for (var i = 1; i <6; i++) {
+    $(`#${i}Date`).text(moment().add(i, "d").format("dddd,MMMM Do"));
+}
