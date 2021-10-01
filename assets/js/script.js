790fc536aa834b0207c0761afc05d1d71187fa84
Luke McMillan <mcmillan.luke@yahoo.com.au>
2021-10-01 15:06:25 +1000

    added event listeners to the Java file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,36 @@
-// set current date in jumbotron
-var today = moment().format("dddd, MMMM Do");
-$("#currentDate").prepend(today);
-console.log(today);
+$(document).ready(function() {
+     var searchHistory = [];   
+    // set current date in jumbotron
+    var today = moment().format("dddd, MMMM Do");
+    $("#currentDate").prepend(today);
+    console.log(today);
 
-// Date for the 5 day forecast
+    // Date for the 5 day forecast
 
-for (var i = 1; i <6; i++) {
-    $(`#${i}Date`).text(moment().add(i, "d").format("dddd,MMMM Do"));
-}
+    for (var i = 1; i <6; i++) {
+        $(`#${i}Date`).text(moment().add(i, "d").format("dddd,MMMM Do"));
+
+    // event listeners
+
+    // Submit event on secrch form
+    $("form").on("submit"), function(event){
+        event.preventDefault();
+        let city = $("input").val();
+        if (city === "") {
+            return;
+        }
+        // Call data from API 
+        call();
+        // 
+    });
+
+    // click event search history
+$(".searchHistoryEl").on("click", function (event) {
+    event.preventDefault();
+    let btnCityName = $(this).text();
+    // call data from API
+    call(btnCityName);
+
+});
+
+
